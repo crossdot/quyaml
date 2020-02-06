0d64600e7092422bb27b7d3eb689d9b24276b0f3
Pavlikov Vitaly <vpavlikov@marathonbet.ru>
2020-02-06 19:33:43 +0300

    yamlEmitter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use yaml_rust::{YamlLoader, Yaml};
+use yaml_rust::{YamlLoader, YamlEmitter, Yaml};
 use clap::Clap;
 use regex::Regex;
 
@@ -36,12 +36,6 @@ services:
     let splitted_path = opts.path.split("/");
     let path: Vec<&str> = splitted_path.collect();
 
-
-    // let mut cur_element = &doc;
-    // let mut path = String::new();
-    // for s in splitted_path {
-    //     println!("{}", s)
-    // }
     find(&doc, path.as_slice(), &[]);
     Ok(())
 }
@@ -49,6 +43,13 @@ services:
 fn find(doc: &Yaml, path: &[&str], sp: &[Yaml]) {
     if path.len() == 0 {
         println!("{:?} = {:?}", &sp, &doc);
+        // Dump the YAML object
+        let mut out_str = String::new();
+        {
+            let mut emitter = YamlEmitter::new(&mut out_str);
+            emitter.dump(doc).unwrap(); // dump the YAML object to a String
+        }
+        println!("{}", out_str);
     } else {
         let key = path[0];
         let re_condition = Regex::new(r"^(.*?)(?:\((.*?)\))?$").unwrap();
