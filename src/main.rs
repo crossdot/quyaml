7596e95de02671da630212c86366904bb435cd99
Pavlikov Vitaly <vpavlikov@marathonbet.ru>
2020-02-06 13:56:12 +0300

    wildcard

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use yaml_rust::{YamlLoader, Yaml};
 use clap::Clap;
+use regex::Regex;
 
 // use std::io::prelude::*;
 // use std::io::{self, BufRead, Read};
@@ -9,8 +10,8 @@ use clap::Clap;
 #[clap(version = "1.0", author = "Pavlikov V.")]
 struct Opts {
     /// Sets a custom config file. Could have been an Option<T> with no default too
-    #[clap(short = "c", long = "condition")]
-    condition: Option<String>,
+    // #[clap(short = "c", long = "condition")]
+    // condition: Option<String>,
     /// Query path
     #[clap(required = true)]
     path: String,
@@ -22,10 +23,10 @@ fn main() -> Result<(), std::io::Error> {
     let s =
 "
 services:
-    -
+    db:
         image: postgres
         scale: 1
-    -
+    front:
         image: nginx
         scale: 0
 ";
@@ -51,17 +52,30 @@ fn find(doc: &Yaml, path: &[&str], sp: &[Yaml]) {
     } else {
         let key = path[0];
         match &doc {
-            Yaml::Hash(ref map) if key != "*" => {
+            Yaml::Hash(ref _map) if !key.contains("*") => {
                 find(&doc[key], &path[1..], &[sp, &[Yaml::String(key.to_owned())]].concat())
             },
-            Yaml::Array(ref array) if key != "*" => {
+            Yaml::Array(ref _array) if key != "*" => {
                 if let Ok(intkey) = key.parse::<usize>() {
                     find(&doc[intkey], &path[1..], &[sp, &[Yaml::Integer(intkey as i64)]].concat())
                 }
             },
-            Yaml::Hash(ref map) => {
+            Yaml::Hash(ref map) if key == "*" => {
                 for entry in map.iter() {
                     find(&entry.1, &path[1..], &[sp, &[entry.0.clone()]].concat());
+                }
+            },
+            Yaml::Hash(ref map) => {
+                let re_str = key.replace("*", ".*?");
+                let re_str = format!("^{}$", re_str);
+                let re = Regex::new(&re_str).unwrap();
+
+                for entry in map.iter() {
+                    if let Yaml::String(s) = entry.0 {
+                        if re.is_match(s) {
+                            find(&entry.1, &path[1..], &[sp, &[entry.0.clone()]].concat());
+                        }
+                    }
                 }
             },
             Yaml::Array(ref array) => {
