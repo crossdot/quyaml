16a6cd897b28242a94849626f3d83155c177008e
Pavlikov Vitaly <vpavlikov@marathonbet.ru>
2020-02-06 17:13:48 +0300

    simple conditions

--- src/main.rs
+++ src/main.rs
@@ -51,20 +51,33 @@ fn find(doc: &Yaml, path: &[&str], sp: &[Yaml]) {
         println!("{:?} = {:?}", &sp, &doc);
     } else {
         let key = path[0];
+        let re_condition = Regex::new(r"^(.*?)(?:\((.*?)\))?$").unwrap();
+        let cap = re_condition.captures(key).unwrap();
+        let mut key = cap.get(1).map_or("*", |m| m.as_str());
+        if key == "" {
+            key = "*";
+        }
+        let condition = &cap.get(2);
+
         match &doc {
-            Yaml::Hash(ref _map) if !key.contains("*") => {
-                find(&doc[key], &path[1..], &[sp, &[Yaml::String(key.to_owned())]].concat())
-            },
+            // Yaml::Hash(ref _map) if !key.contains("*") => {
+            //     find(&doc[key], &path[1..], &[sp, &[Yaml::String(key.to_owned())]].concat())
+            // },
             Yaml::Array(ref _array) if key != "*" => {
                 if let Ok(intkey) = key.parse::<usize>() {
+                    if !check(&doc[intkey], &condition) {
+                        return;
+                    }
                     find(&doc[intkey], &path[1..], &[sp, &[Yaml::Integer(intkey as i64)]].concat())
                 }
             },
-            Yaml::Hash(ref map) if key == "*" => {
-                for entry in map.iter() {
-                    find(&entry.1, &path[1..], &[sp, &[entry.0.clone()]].concat());
-                }
-            },
+
+            // Yaml::Hash(ref map) if key == "*" => {
+            //     for entry in map.iter() {
+            //         find(&entry.1, &path[1..], &[sp, &[entry.0.clone()]].concat());
+            //     }
+            // },
+
             Yaml::Hash(ref map) => {
                 let re_str = key.replace("*", ".*?");
                 let re_str = format!("^{}$", re_str);
@@ -73,18 +86,97 @@ fn find(doc: &Yaml, path: &[&str], sp: &[Yaml]) {
                 for entry in map.iter() {
                     if let Yaml::String(s) = entry.0 {
                         if re.is_match(s) {
+                            if !check(&entry.1, &condition) {
+                                continue;
+                            }
                             find(&entry.1, &path[1..], &[sp, &[entry.0.clone()]].concat());
                         }
                     }
                 }
             },
+
             Yaml::Array(ref array) => {
                 for (i, v) in array.iter().enumerate() {
+                    if !check(&v, &condition) {
+                        continue;
+                    }
                     find(&v, &path[1..], &[sp, &[Yaml::Integer(i as i64)]].concat());
                 }
             },
             _ => {
 
+            }
+        }
+    }
+}
+
+fn check(doc: &Yaml, condition: &Option<regex::Match>) -> bool {
+    if let Some(condition_match) = condition {
+        let condition_str = condition_match.as_str();
+        let re = Regex::new(r"^\s*(.*?)\s*(=|==|!=|>|<)\s*(.*?)$").unwrap();
+        let cap = re.captures(&condition_str).unwrap();
+
+        let l = cap.get(1).map_or("", |m| m.as_str());
+        let e = cap.get(2).map_or("", |m| m.as_str());
+        let r = cap.get(3).map_or("", |m| m.as_str());
+
+        let value = get(&doc, &[l]);
+        match value {
+            Yaml::Integer(a) => {
+                if let Ok(b) = r.parse::<i64>() {
+                    match e {
+                        "=" | "==" => {
+                            return *a == b
+                        },
+                        "!=" => {
+                            return *a != b
+                        },
+                        ">" => {
+                            return *a > b
+                        },
+                        "<" => {
+                            return *a < b
+                        },
+                        _ => {
+                            return false;
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            },
+            _ => {
+                return false;
+            },
+        }
+
+        // if let Yaml::Integer(v) = value {
+        //     if *v != r {
+        //         return false;
+        //     }
+        // }
+    }
+    return true;
+}
+
+fn get<'a>(doc: &'a Yaml, path: &[&str]) -> &'a Yaml {
+    if path.len() == 0 {
+        &doc
+    } else {
+        let key = path[0];
+        match &doc {
+            Yaml::Array(ref _array) if key != "*" => {
+                if let Ok(intkey) = key.parse::<usize>() {
+                    get(&doc[intkey], &path[1..])
+                } else {
+                    &Yaml::BadValue
+                }
+            },
+            Yaml::Hash(ref _map) if !key.contains("*") => {
+                get(&doc[key], &path[1..])
+            },
+            _ => {
+                &Yaml::BadValue
             }
         }
     }
